f21814a526b594186fdc077ff1acfc0948377cc6
chemuto <chemuto@gmail.com>
2014-09-12 08:58:55 +0800

    fixed controls for search domain tool

--- client/views/search-results/search_result_list.js
+++ client/views/search-results/search_result_list.js
@@ -1,17 +1,5 @@
 Template.searchResultList.helpers({
   searchResults: function() {
-    //console.log("SearchResults: ", SearchResults.find().fetch());
-    // if (SearchResults.find().count() === 0) {
-    //     console.log("SearchResults.find().count() === 0");
-    //     for (var i = 0; i < 3; i++) {
-    //         SearchResults.insert(
-    //           {
-    //             title: 'testing'+i,
-    //             projectId: Session.get('currentProjectId')
-    //           }
-    //         );
-    //     };
-    // }
     return SearchResults.find();
   }
 
