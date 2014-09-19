ab0d65c94e2a747fabf8205736f3a7f897346f7b
chemuto <chemuto@gmail.com>
2014-09-19 19:14:04 +0800

    added project header html. reduced container max width to 992px

---
+++ client/views/projects/project_header.js
@@ -0,0 +1,10 @@
+Template.projectHeader.helpers({
+    tabClass: function (tab) {
+        if(this.tab == tab){
+            return 'active'
+        } else {
+            return ''
+        }
+    },
+    
+});
--- client/views/projects/project_page.js
+++ client/views/projects/project_page.js
@@ -1,27 +1,11 @@
 Template.projectPage.helpers({
-    tabClass: function (tab) {
-        if(this.tab == tab){
+    pillClass: function (pill) {
+        if(this.pill == pill){
             return 'active'
         } else {
             return ''
         }
     },
-    // postsWithRank: function() {
-    //     this.posts.rewind();
-    //     return this.posts.map(function(post, index, cursor) {
-    //       post._rank = index;
-    //       return post;
-    //   });
-    // }
 });
 
-// Template.projectPage.events({
-//     'click .remove-positions': function(){
-//         console.log("click .remove-positions");
-//         Positions.remove({});
-//         console.log(" Positions.remove({});");
-//         console.log(" Positions: ", Positions.find().fetch());
-//     }
-
-// });
 
